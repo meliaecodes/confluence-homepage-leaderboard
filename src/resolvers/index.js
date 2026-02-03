d41fb24a8258a12b76e4232241dcb2c9efa837bc
Melissa Paisley <mpaisley@atlassian.com>
2026-02-03 14:36:30 +1100

    added webtrigger

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,5 +1,28 @@
 import Resolver from '@forge/resolver';
 
+export const storeMessage = async (event) => {
+  //TODO - error handling
+  let eventArray = await kvs.get(event.event);
+  if(eventArray) {
+    await kvs.set(event.event, [...eventArray, event.name]);
+    return ({
+      outputKey: "status-ok",
+      message: "updated existing event"
+    })
+  }
+  else {
+    await kvs.set(event.event, [event.name]);
+    return ({
+      outputKey: "status-ok",
+      message: "created new event"
+    })
+  }
+  return ({
+    outputKey: "status-error",
+    message: "error storing data"
+  })
+}
+
 const resolver = new Resolver();
 
 resolver.define('getText', (req) => {
